7eda7c23e82a32e6742e3c87219e648eecf7c84c
Dominik Makus <dominik@makus-home.de>
2018-02-03 19:46:04 +0100

    project started

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main (){
+    unsigned long int number;
+    unsigned long int prime = 2;
+    bool print;
+
+    std::cout << "Enter how many prime number you would like to get:" << std::endl;
+    std::cin >> number;
+
+    for (unsigned long int i = 1; i <= number; ){
+        print = true;
+        if(prime == 2){
+            std::cout << i << ". - " << prime << std::endl;
+            i++;
+            prime++;
+            print = false;
+        } else {
+            for(unsigned long int z = 3; z < prime; z += 2){
+                if (prime % z == 0){
+                    print = false;
+                }
+            }
+            if (print){
+                std::cout << i << ". - " << prime << std::endl;
+                i++;
+            }
+            prime += 2;
+        }
+    }
+}
